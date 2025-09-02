381f77ea25becbff5ff212cc72ec5d61b6426cfa
Ken Esparta <kenesparta@pm.me>
2025-09-02 14:42:54 -0500

    feat: Adding navbar and button components

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,33 +1,9 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import Navbar from "./components/ui/Navbar";
 
 const DashboardLayout: React.FC = () => {
   return (
-    <div className="">
-      <nav className="menu">
-        <ul>
-          <li>
-            <Link to="/dashboard">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/postulante">Postulante</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/evaluacion">Evaluación</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/examen">Examen</Link>
-          </li>
-          <li>
-            <Link to="/login">Salir</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <section className="content">
-        <Outlet />
-      </section>
-    </div>
+    <Navbar />
   );
 };
 
---
+++ src/components/ui/Navbar/Navbar.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import styles from './Navbar.module.css';
+
+const Navbar: React.FC = () => {
+  return (
+    <div className={styles.navbarLayout}>
+      {/* Left Sidebar */}
+      <aside className={styles.sidebar}>
+        <div className={styles.sidebarHeader}>
+          <h1 className={styles.sidebarTitle}>Quiz Dashboard</h1>
+        </div>
+
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <NavLink
+                to="/dashboard"
+                className={styles.navLink}
+                end
+              >
+                Inicio
+              </NavLink>
+            </li>
+            <li className={styles.navItem}>
+              <NavLink
+                to="/dashboard/postulante"
+                className={styles.navLink}
+              >
+                Postulante
+              </NavLink>
+            </li>
+            <li className={styles.navItem}>
+              <NavLink
+                to="/dashboard/evaluacion"
+                className={styles.navLink}
+              >
+                Evaluación
+              </NavLink>
+            </li>
+            <li className={styles.navItem}>
+              <NavLink
+                to="/dashboard/examen"
+                className={styles.navLink}
+              >
+                Examen
+              </NavLink>
+            </li>
+            <li className={styles.navItem}>
+              <NavLink
+                to="/login"
+                className={styles.navLink}
+              >
+                Salir
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+
+      {/* Main Content Area */}
+      <main className={styles.content}>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Navbar;
